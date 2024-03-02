e3071d7d2c5566abfad1820690f9c9cd77b1aeb9
markangeloracca <markangelo.racca@gmail.com>
2024-03-02 19:31:07 +0800

    Recon App Initital

---
+++ src/main/java/ph/oron/reconwarior/ReconWariorApplication.java
@@ -0,0 +1,13 @@
+package ph.oron.reconwarior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconWariorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconWariorApplication.class, args);
+	}
+
+}
